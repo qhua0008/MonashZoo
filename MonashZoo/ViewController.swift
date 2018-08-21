191fd1bd8057a397e2993a1b4ee201721bbf1bb4
Qianyi Huang <qhuang@dyn-118-139-22-122.its.monash.edu.au>
2018-08-21 19:49:21 +1000

    Initial Commit

---
+++ MonashZoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MonashZoo
+//
+//  Created by Qianyi Huang on 21/8/18.
+//  Copyright © 2018 Qianyi Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
